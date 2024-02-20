7d82bc3bfdd63ae44303882f2b197605f6918c33
mihkelroomet <mihkelroomet@gmail.com>
2024-02-20 21:43:01 +0200

    Added game time

---
+++ globals/globals.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var game_time : int = Time.get_unix_time_from_datetime_string("2024-09-02T08:00:00")
+
+var GAME_TIME_SPEED : int = 200
---
+++ ui/time_and_date.gd
@@ -0,0 +1,44 @@
+extends VBoxContainer
+
+@onready var time_label : Label = $Time
+@onready var day_of_week_label : Label = $DayOfWeek
+@onready var day_of_year_label : Label = $DayOfYear
+@onready var timer : Timer = $Timer
+
+var WEEKDAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var MONTHS = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+func set_time(unix_time):
+	var datetime_dict = Time.get_datetime_dict_from_unix_time(unix_time)
+	time_label.text = get_time_from_datetime_dict(datetime_dict)
+	day_of_week_label.text = WEEKDAYS[datetime_dict["weekday"]]
+	day_of_year_label.text = get_day_of_year_from_datetime_dict(datetime_dict)
+
+func _on_timer_timeout():
+	Globals.game_time += timer.wait_time * Globals.GAME_TIME_SPEED
+	set_time(Globals.game_time)
+	
+func get_time_from_datetime_dict(datetime_dict):
+	var hour = datetime_dict["hour"]
+	var minute = str(datetime_dict["minute"]).pad_zeros(2)
+	
+	var suffix = "AM"
+	
+	if (hour >= 12):
+		suffix = "PM"
+		if (hour >= 13):
+			hour -= 12
+	
+	if (hour == 0):
+		hour = 12
+	
+	hour = str(hour)
+	
+	return hour + ":" + minute + " " + suffix
+
+func get_day_of_year_from_datetime_dict(datetime_dict):
+	var month = MONTHS[datetime_dict["month"]]
+	var day = str(datetime_dict["day"])
+	var year = str(datetime_dict["year"])
+	
+	return month + " " + day + " " + year
