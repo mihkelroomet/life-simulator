b3725f4a07a7f9aa40f896f5162c8d7966e36b69
mihkelroomet <mihkelroomet@gmail.com>
2024-04-05 18:44:05 +0300

    Removed cyclic dependency between activity_select_button.tscn and activity_start_panel.gd which was causing corruption in editor

--- ui/activity_duration_vbox.gd
+++ ui/activity_duration_vbox.gd
@@ -2,6 +2,8 @@ extends VBoxContainer
 
 signal set_activity_start_panel_selected_duration(duration : int)
 
+const ActivityStartPanel = preload("res://ui/activity_start_panel.gd")
+
 @onready var activity_duration_label = $ActivityDurationLabel
 @onready var activity_duration_slider = $ActivityDurationSlider
 
@@ -32,23 +34,24 @@ func _on_set_activity_start_panel_visible(if_visible : bool, _activities : Array
 		activity_duration_label.text = "?h ?min"
 
 func _on_set_activity_start_panel_selected_activity(activity : Globals.Activity):
-	var activity_data = Globals.get_activity_data(activity)
-	
-	var min_quarters_of_hour = round(activity_data.min_duration / 0.25)
-	var max_quarters_of_hour = round(activity_data.max_duration / 0.25)
-	var default_quarters_of_hour = round(activity_data.default_duration / 0.25)
-	
-	activity_duration_slider.min_value = min_quarters_of_hour
-	activity_duration_slider.max_value = max_quarters_of_hour
-	activity_duration_slider.value = default_quarters_of_hour
-	
-	set_duration(default_quarters_of_hour)
-	
-	# Disable slider if there is only one possible duration, enable it otherwise
-	if min_quarters_of_hour == default_quarters_of_hour and default_quarters_of_hour == max_quarters_of_hour:
-		activity_duration_slider.editable = false
-	else:
-		activity_duration_slider.editable = true
+	if activity != ActivityStartPanel.selected_activity:
+		var activity_data = Globals.get_activity_data(activity)
+		
+		var min_quarters_of_hour = round(activity_data.min_duration / 0.25)
+		var max_quarters_of_hour = round(activity_data.max_duration / 0.25)
+		var default_quarters_of_hour = round(activity_data.default_duration / 0.25)
+		
+		activity_duration_slider.min_value = min_quarters_of_hour
+		activity_duration_slider.max_value = max_quarters_of_hour
+		activity_duration_slider.value = default_quarters_of_hour
+		
+		set_duration(default_quarters_of_hour)
+		
+		# Disable slider if there is only one possible duration, enable it otherwise
+		if min_quarters_of_hour == default_quarters_of_hour and default_quarters_of_hour == max_quarters_of_hour:
+			activity_duration_slider.editable = false
+		else:
+			activity_duration_slider.editable = true
 
 func _on_activity_duration_slider_value_changed(value):
 	set_duration(value)
--- ui/activity_select_button.gd
+++ ui/activity_select_button.gd
@@ -2,13 +2,10 @@ extends CheckBox
 
 signal set_activity_start_panel_selected_activity(activity : Globals.Activity)
 
-const ActivityStartPanel = preload("res://ui/activity_start_panel.gd")
-
 @export var activity : Globals.Activity
 
 func _ready():
 	set_activity_start_panel_selected_activity.connect(GameManager._on_set_activity_start_panel_selected_activity)
 
 func _on_button_pressed():
-	if activity != ActivityStartPanel.selected_activity:
-		set_activity_start_panel_selected_activity.emit(activity)
+	set_activity_start_panel_selected_activity.emit(activity)
