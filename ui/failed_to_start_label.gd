4f6eb6895cae6fa2135396fc2f937d37d4d4f2df
mihkelroomet <mihkelroomet@gmail.com>
2024-05-06 03:21:10 +0300

    Fixed red level error shown on activity select panel reopen if it hadn't faded yet

--- ui/failed_to_start_label.gd
+++ ui/failed_to_start_label.gd
@@ -4,10 +4,15 @@ extends Label
 
 func _ready():
 	Events.fail_to_start_activity.connect(_on_failed_to_start_activity)
+	Events.set_activity_start_panel_visible.connect(_on_set_activity_start_panel_visible)
 
 func _on_failed_to_start_activity():
 	visible = true
 	failed_to_start_label_timer.start()
+
+func _on_set_activity_start_panel_visible(p_visible : bool, _activities : Array[ActivityManager.Activity]):
+	if p_visible:
+		visible = false
 
 func _on_timer_timeout():
 	visible = false
