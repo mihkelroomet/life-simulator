36c34d587198162500b4fef97402693140473f35
mihkelroomet <mihkelroomet@gmail.com>
2024-05-10 06:06:24 +0300

    Added post-game dialogue

--- autoload/thesis_writing_manager.gd
+++ autoload/thesis_writing_manager.gd
@@ -7,6 +7,9 @@ const THESIS_WRITING_SPEED : float = 0.0125 # Current value means 80h for whole 
 
 const THESIS_WRITTEN_INITIAL_VALUE : float = 0.4
 
+var grade_thresholds : Array[float] = [0.51, 0.61, 0.71, 0.81, 0.91, 10000.0]
+var grades : Array[String] = ["F", "E", "D", "C", "B", "A"]
+
 var thesis_written : float
 
 func _ready():
@@ -22,6 +25,11 @@ func write_thesis(game_hours_elapsed):
 	thesis_written += game_hours_elapsed * THESIS_WRITING_SPEED
 	thesis_written = clamp(thesis_written, 0.0, 1.0)
 	thesis_written_amount_changed.emit(thesis_written)
+
+func get_grade():
+	for i in range(grade_thresholds.size()):
+		if thesis_written < grade_thresholds[i]:
+			return grades[i]
 
 func _on_start_game():
 	thesis_written = THESIS_WRITTEN_INITIAL_VALUE
--- ui/thesis_writing_panel.gd
+++ ui/thesis_writing_panel.gd
@@ -3,9 +3,6 @@ extends MarginContainer
 @onready var progress_bar = $VBoxContainer/ThesisWritingBar/MarginContainer/VBoxContainer/MarginContainer/ProgressBar
 @onready var progress_percentage_label = $VBoxContainer/ThesisWritingBar/MarginContainer/VBoxContainer/MarginContainer/ProgressBar/ProgressPercentageLabel
 
-var grade_thresholds : Array[float] = [51.0, 61.0, 71.0, 81.0, 91.0, 10000.0]
-var grades : Array[String] = ["F", "E", "D", "C", "B", "A"]
-
 func _ready():
 	Events.thesis_written_amount_changed.connect(_on_thesis_written_amount_changed)
 
@@ -13,15 +10,6 @@ func _on_thesis_written_amount_changed(new_value : float):
 	set_progress_bar_value(new_value)
 
 # Sets the value of the progress bar. The input value should be in range [0.0, 1.0].
-func set_progress_bar_value(new_value):
-	progress_bar.value = progress_bar.min_value + new_value * (progress_bar.max_value - progress_bar.min_value)
-
-func _on_progress_bar_value_changed(value):
-	var grade : String
-	
-	for i in range(grade_thresholds.size()):
-		if value < grade_thresholds[i]:
-			grade = grades[i]
-			break
-	
-	progress_percentage_label.text = str(value, "% (" + grade + ")")
+func set_progress_bar_value(value):
+	progress_bar.value = progress_bar.min_value + value * (progress_bar.max_value - progress_bar.min_value)	
+	progress_percentage_label.text = str(progress_bar.value, "% (" + ThesisWritingManager.get_grade() + ")")
